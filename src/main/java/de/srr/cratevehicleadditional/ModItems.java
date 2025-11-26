a4a35e1e2ffd116eb44b560d709d8fac6424f646
WhenLifeGivesYouOrangesMakeOrangeJuice <seb2022cool@gmail.com>
2025-11-26 16:32:32 +0100

    Config Changes

---
+++ src/main/java/de/srr/cratevehicleadditional/ModItems.java
@@ -0,0 +1,21 @@
+package de.srr.cratevehicleadditional;
+
+import net.minecraft.world.item.Item;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.registries.DeferredItem;
+import net.neoforged.neoforge.registries.DeferredRegister;
+
+public class ModItems{
+
+    public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(CreateVehicleAdditional.MOD_ID);
+
+    public static final DeferredItem<Item> CARBON = ITEMS.registerItem(
+            "carbon",
+            Item::new, // The factory that the properties will be passed into.
+            new Item.Properties() // The properties to use.
+    );
+    public static void register(IEventBus eventBus){
+        ITEMS.register(eventBus);
+    }
+
+}
